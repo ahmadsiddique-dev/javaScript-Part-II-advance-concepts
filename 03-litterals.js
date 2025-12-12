ff9dbb2552d5bdbf209fb31478c565b76920090e
Ahmad Siddique <asshikrani66@gmail.com>
2025-12-12 18:16:10 +0500

    commas in javaScript

---
+++ 03-litterals.js
@@ -0,0 +1,10 @@
+let arr = ["colombia", "NYC", "California"]
+
+// console.log(arr.length);
+
+const fish = ["Lion", , "Angel"];
+
+// Skips in map function but when we access as fish[0]
+// it gives undefined btw it is not undefined it is <empty Item>
+
+console.log(fish);
