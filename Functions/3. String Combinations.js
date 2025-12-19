7e3062c79966b2ea4d415e51ec29b428191021ed
Ahmad Siddique <asshikrani66@gmail.com>
2025-12-19 11:30:55 +0500

    String Combinatin exercise, Oh btw I solved it by myself.

--- Functions/3. String Combinations.js
+++ Functions/3. String Combinations.js
@@ -5,3 +5,20 @@
 // Expected Output : d,do,dog,o,og,g
 
 
+function main(str) {
+    let res = [];
+
+    if (typeof str !== "string" || str.trim() === "") return "Invalid input";
+
+    if (str.length === 1) return str;
+
+    for(let i = 0; i < str.length; i++) { // We got first digit like d
+        res.push(str[i]);
+        for (let j = i + 1; j < str.length; j++) { // we got o, then g here 
+            res.push(str.substring(i, j+1));
+        }
+    }
+    return res.toString();
+}
+
+console.log(main("dog"));
